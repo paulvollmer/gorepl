003bf5d20a8e7dc3ff2609e3c8eb5ba67d3b9c01
paulvollmer <paul.vollmer@fh-potsdam.de>
2015-10-13 09:39:36 +0200

    Create basic example

---
+++ examples/basic/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/paulvollmer/gorepl"
+
+func main() {
+	// Initialize a new REPL instance.
+	app := repl.NewREPL("SAMPLE")
+	app.Version = "0.0.1"
+	app.Author = "Paul Vollmer"
+
+	// Set the commands and the handlers.
+	app.Command("hello", "some words about the 'hello' command", func(ctx repl.Context) {
+		ctx.Writeln(ctx.Keyword, "world")
+	})
+
+	// Run the REPL as an infinite loop.
+	app.Run()
+}
